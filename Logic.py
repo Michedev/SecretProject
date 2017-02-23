855db3d8253cd8950f74130aabf445e59fd4ce3f
Michele De Vita <mdv1994@gmail.com>
2017-02-23 19:37:39 +0100

    Write logic

---
+++ Logic.py
@@ -0,0 +1,20 @@
+from CacheServer import CacheServer
+from TheParser import TheConfigParser
+
+
+class Brain:
+
+    def __init__(self, filename):
+        self.parser = TheConfigParser(filename)
+        self.parser.readConfiguration()
+        self.output = open("output.out")
+
+    def run(self):
+        CacheServer.capacity = self.parser.cacheServerCapacity
+        sortedVideos = self.parser.videos.sort(key=lambda video: video.size)
+        sortedRequests = self.parser.requests.sort(key=lambda req: req.numRequests)[-1:]
+        self.output.write(self.parser.numberOfCacheServer)
+        for i in range(self.parser.numberOfCacheServer):
+            idVideo = sortedRequests[i].videoID
+            video = self.parser.videos[i]
+            self.output.write(f"{i} {idVideo}")
