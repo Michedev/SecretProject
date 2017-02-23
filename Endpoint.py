eeef1ca7486def7f9affdad7feca3367ec4ab8de
Michele De Vita <mdv1994@gmail.com>
2017-02-23 19:16:03 +0100

    Continue building classes

--- Endpoint.py
+++ Endpoint.py
@@ -3,7 +3,11 @@ from CacheServer import CacheServer
 class Endpoint:
 
     def __init__(self, latency_to_datacenter, cacheServersList):
+        self.latencyToDatacenter = latency_to_datacenter
         self.cacheServerList = [CacheServer(id, latency) for id, latency in cacheServersList]
 
-    def __getitem__(self, item):
-        return self.cacheServerList
+    def __getitem__(self, idCacheServer):
+        for cacheServer in self.cacheServerList:
+            if idCacheServer == cacheServer.id:
+                return cacheServer
+        raise Exception("ID Server Not found")
---
+++ EndpointRequestes.py
@@ -0,0 +1,6 @@
+class EndpointRequestes:
+
+    def __init__(self, numRequestes, videoID, endpointID):
+        self.num = numRequestes
+        self.videoID = videoID
+        self.endpointID = endpointID
