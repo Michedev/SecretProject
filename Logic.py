a55414057dfe8fcb9aa50d9425979416f057beb2
Michele De Vita <mdv1994@gmail.com>
2017-02-23 20:28:30 +0100

    Fix logic bug #3

--- Logic.py
+++ Logic.py
@@ -10,6 +10,7 @@ class Brain:
         self.parser.readConfiguration()
         self.printer = ThePrinter(self.parser.numberOfCacheServer, outputFilename)
         self.cacheServerActualCapacity = [0] * self.parser.numberOfCacheServer
+        self.videosInCacheServer = dict([(i, []) for i in range(self.parser.numberOfCacheServer)])
 
 
     def run(self):
@@ -22,13 +23,17 @@ class Brain:
 
 
     def putInCacheServer(self, request):
+
         endpoint = self.parser.endpoints[request.endpointID]
         video = self.parser.videos[request.videoID]
         availableCacheServer = list(filter(lambda cacheServer : self.cacheServerActualCapacity[cacheServer.id] + video.size < CacheServer.capacity, endpoint.cacheServerList))
         if len(availableCacheServer) == 0:
             return None
         bestCacheServer = min(availableCacheServer, key=lambda cacheServer: cacheServer.latency)
+        if video.id in self.videosInCacheServer[bestCacheServer.id] :
+            return None
         self.printer.put(bestCacheServer.id, request.videoID)
         self.cacheServerActualCapacity[bestCacheServer.id] += video.size
+        self.videosInCacheServer[bestCacheServer.id].append(video.id)
 
 
